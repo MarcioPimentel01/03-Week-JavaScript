697d3122eae01b43ca51d375ffe5f9870b524bd0
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-03-20 06:23:51 -0400

    added an evaluation expression invoking a function - variables

--- 01-Activities/04-Stu_Hello-Variable/Solved/script.js
+++ 01-Activities/04-Stu_Hello-Variable/Solved/script.js
@@ -1,19 +1,43 @@
-// Declare variables using let
-let personName = 'Sakura';
-let pets = 3;
-let funFact = 'I like pineapple on my pizza.';
+let studentName = "Marcio"
+console.log(studentName);
 
-// Use template literals to combine data and variable names in a single console log
-console.log(`My name is ${personName}.`);
-console.log(`I have ${pets} pet(s).`);
-console.log(`Fun fact: ${funFact}`);
+let studentAge = 47;
+console.log(studentAge);
 
-// When re-assigning variables, use variable name
-personName = 'Mateo';
-pets = 5;
-funFact = 'I was a yo-yo champ in third grade.';
+const teacherName = "Stanley";
+console.log(teacherName);
 
-// Logs message with re-assigned values
-console.log(`My name is ${personName}.`);
-console.log(`I have ${pets} pet(s).`);
-console.log(`Fun fact: ${funFact}`);
+studentName = "Pimentel";
+studentAge = 48;
+
+console.log(studentName + " " + studentAge);
+
+
+// the following is very important
+//Evaluation expression
+function greet() {
+    return "Hello, world!";
+}
+
+let add = `${greet}`;
+console.log(add);
+
+// // Declare variables using let
+// let personName = 'Sakura';
+// let pets = 3;
+// let funFact = 'I like pineapple on my pizza.';
+
+// // Use template literals to combine data and variable names in a single console log
+// console.log(`My name is ${personName}.`);
+// console.log(`I have ${pets} pet(s).`);
+// console.log(`Fun fact: ${funFact}`);
+
+// // When re-assigning variables, use variable name
+// personName = 'Mateo';
+// pets = 5;
+// funFact = 'I was a yo-yo champ in third grade.';
+
+// // Logs message with re-assigned values
+// console.log(`My name is ${personName}.`);
+// console.log(`I have ${pets} pet(s).`);
+// console.log(`Fun fact: ${funFact}`);
