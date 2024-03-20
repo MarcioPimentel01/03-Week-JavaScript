ac7821b6c9f49792e48e379aadd7186221141928
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-03-20 03:05:59 -0400

    Logical Comparission activity, finished

--- 01-Activities/08-Stu_Logical-Comparison-Operators/Unsolved/script.js
+++ 01-Activities/08-Stu_Logical-Comparison-Operators/Unsolved/script.js
@@ -8,17 +8,18 @@ const expression1 = (b === e);
 const expression2 = (e < d);
 
 // Use comparison operators so all expressions below log to the console as true
-console.log(a === b);
+console.log(a === b); // Check if the value of 'a' is exactly equal to the value of 'b', if true, log true, otherwise false.
 // false
-console.log(b !== e);
+console.log(b !== e); // Check if the value of 'b' is not equal to the value of 'e', if true, log true, otherwise false.
 // false
-console.log(c < b);
+console.log(c < b); // Check if the value of 'c' is less than the value of 'b', if true, log true, otherwise false.
 // false
-console.log(d > 0);
+console.log(d > 0); // Check if the value of 'd' is greater than 0, if true, log true, otherwise false.
 // false
 
 // Use logical operators so all expressions below log to the console as true
-console.log(expression1 && expression2);
+console.log(expression1 && expression2); // Log true if both 'expression1' and 'expression2' are true, otherwise false.
 // false
-console.log( !expression1 || expression2);
+console.log( !expression1 || expression2); // Log true if either 'expression1' is false or 'expression2' is true, otherwise false.
 // false
+
