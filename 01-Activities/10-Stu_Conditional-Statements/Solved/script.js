bc101e1a5f184cee4722b7fc165e6d0a5a4e3317
Marcio Pimentel <149727150+MarcioPimentel01@users.noreply.github.com>
2024-03-21 22:34:01 -0400

    solved activities commit

--- 01-Activities/10-Stu_Conditional-Statements/Solved/script.js
+++ 01-Activities/10-Stu_Conditional-Statements/Solved/script.js
@@ -1,7 +1,7 @@
-// Change the values and operators below to test your algorithm meets all conditions
-const x = 50;
-const expression1 = (x < 25);
-const expression2 = (x > 50);
+// // Change the values and operators below to test your algorithm meets all conditions
+// const x = 50;
+// const expression1 = (x < 25);
+// const expression2 = (x > 50);
 
 // Write Your JavaScript Code Here
 // Check if both expressions are true using &&
@@ -21,3 +21,32 @@ if(expression1 && expression2) {
 } else {
     console.log("False ❌ False ❌");
 }
+
+// let hungryLevel = 50;
+// let isLunchTime = true;
+// let lunchBill = 17;
+
+// console.log(isLunchTime === true);
+
+// if (true) console.log("I'm testing");
+
+// if (isLunchTime === true) {
+//     console.log ("im'm testing");
+//     console.log("I'm learning");
+// } else console.log(`I will ear later`);
+
+// if (hungryLevel > 51) {
+//     console.log(`I'm getting hungy`);
+// }   else console.log(`not hungry at all`);
+
+// if (lunchBill > 50) {
+//     console.log(`the average price is $`);
+// } else if(lunchBill >= 12 && lunchBill <= 16) {
+//     console.log(`the average price is $$`);
+// }   else if(lunchBill >= 17 && lunchBill <= 22) {
+//     console.log(`the average price is $$$`);
+// }   else if(lunchBill >= 23 && lunchBill <= 30) {
+//     console.log(`the average price is $$$$`);
+// }   else {
+//     console.log(`the average price is caro pra caralho`);
+// }
--- 01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ 01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -6,8 +6,19 @@ const expression2 = (x > 50);
 // Write Your JavaScript Code Here
 
 if (expression1 && expression2) {
-    console.log("True");
-} 
-if(expression1 && expression2) {
     console.log("True ✅ True ✅");
+} else if (expression1 && !expression2) {
+    console.log("True ✅ False ❌");
+} else if (!expression1 && expression2) {
+    console.log("False ❌ True ✅");
+} else {
+    console.log("False ❌ False ❌");
 }
+
+//It's done when the message "True ✅ True ✅" is logged when both expression1 and expression2 are true.
+
+//It's done when the message "True ✅ False ❌" is logged when expression1 is true.
+
+//It's done when the message "False ❌ True ✅" is logged when expression2 is true.
+
+//It's done when the message "False ❌ False ❌" is logged when both expression1 and expression2 are false.
